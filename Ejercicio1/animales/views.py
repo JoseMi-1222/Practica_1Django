4894f17c2fb8de9b9149a285c834bbdfacef8816
Jose Miguel Arras <jose.arras.gavira.al@iespoligonosur.org>
2025-09-30 13:25:38 +0200

    optimizar codigo

--- Ejercicio1/animales/views.py
+++ Ejercicio1/animales/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-from .models import Animal
+from .models import Animal, Protectora, Colaborador
 
 def inicio(request):
     return render(request, 'animals/inicio.html')
@@ -12,11 +11,9 @@ def lista_animales(request):
     return render(request, 'animals/lista_animales.html', {'lista_de_animales': animales})
 
 def lista_protectoras(request):
-    from .models import Protectora
     protectoras = Protectora.objects.all()
     return render(request, 'animals/lista_protectoras.html', {'lista_de_protectoras': protectoras})
 
 def lista_colaboradores(request):
-    from .models import Colaborador
     colaboradores = Colaborador.objects.all()
     return render(request, 'animals/lista_colaboradores.html', {'lista_de_colaboradores': colaboradores})
