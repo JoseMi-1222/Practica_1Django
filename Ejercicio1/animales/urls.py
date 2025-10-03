26040bec29ed92adf000e0fec507116bd77ed373
Jose Miguel Arras <jose.arras.gavira.al@iespoligonosur.org>
2025-10-03 10:33:25 +0200

    Fix formatting in urls.py and add INTERNAL_IPS to settings.py for local development

--- Ejercicio1/animales/urls.py
+++ Ejercicio1/animales/urls.py
@@ -3,7 +3,7 @@ from .views import inicio, lista_animales, lista_protectoras, lista_colaboradore
 
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('animales/',lista_animales, name='lista_animales'),
+    path('animales/', lista_animales, name='lista_animales'),
     path('protectoras/', lista_protectoras, name='lista_protectoras'),
     path('colaboradores/', lista_colaboradores, name='lista_colaboradores'),
 ]
--- Ejercicio2/mysite/settings.py
+++ Ejercicio2/mysite/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
+INTERNAL_IPS = ['127.0.0.1']
+
 
 # Application definition
 
