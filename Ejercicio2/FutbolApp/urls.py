49a8ecc58f35eb93c7524171307366bb2c5a429a
Jose Miguel Arras <jose.arras.gavira.al@iespoligonosur.org>
2025-10-03 08:59:30 +0200

    Urls modificadas

--- Ejercicio2/FutbolApp/urls.py
+++ Ejercicio2/FutbolApp/urls.py
@@ -1,26 +1,5 @@
-from django.shortcuts import render
-from .models import Equipo, Jugador, Partido
 from django.urls import path
-from . import views
-
-# Vista para la página de inicio
-def inicio(request):
-    return render(request, 'futbol/inicio.html')    
-
-# Vista para listar todos los equipos
-def lista_equipos(request):
-    equipos = Equipo.objects.all()
-    return render(request, 'futbol/lista_equipos.html', {'lista_de_equipos': equipos})  
-
-# Vista para listar todos los jugadores
-def lista_jugadores(request):
-    jugadores = Jugador.objects.all()
-    return render(request, 'futbol/lista_jugadores.html', {'lista_de_jugadores': jugadores})
-
-# Vista para listar todos los partidos
-def lista_partidos(request):
-    partidos = Partido.objects.all()
-    return render(request, 'futbol/lista_partidos.html', {'lista_de_partidos': partidos})
+from .views import inicio, lista_equipos, lista_jugadores, lista_partidos
 
 urlpatterns = [
     path('', inicio, name='inicio'),
